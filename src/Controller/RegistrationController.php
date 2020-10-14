7216b0df6e2369a86461076931dcf7633db3a559
lemi diriba <lemidiriba4@gmail.com>
2020-10-14 12:14:03 +0300

    user registration is done

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegistrationController extends AbstractController
@@ -12,19 +13,40 @@ class RegistrationController extends AbstractController
     /**
      * @Route("/registration", name="registration")
      */
-    public function index()
+    public function index(Request $request)
     {
-
         $user = new User();
-
+        $user->setRoles(["ROLE_USER"]);
         $form = $this->createForm(RegistrationType::class, $user);
 
-        return $this->render(
-            'registration/index.html.twig',
-            [
-                'controller_name' => 'RegistrationController',
-                'form' => $form->createView(),
-            ]
-        );
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $user = $form->getData();
+
+
+            dump($user);
+            die();
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($user);
+            $entityManager->flush();
+
+            $this->redirectToRoute(
+                'app_login',
+                [
+                    'user' => $user
+                ]
+            );
+        } else {
+            return $this->render(
+                'registration/index.html.twig',
+                [
+                    'controller_name' => 'RegistrationController',
+                    'form' => $form->createView(),
+                ]
+            );
+        }
     }
 }
